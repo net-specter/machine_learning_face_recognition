ba83be2d644940fed72ebcbc510d8b7e4e0f94e5
net-specter <kaovichet11@gmail.com>
2025-12-25 14:03:42 +0700

    testing ci/cd

--- backend/auth-service/src/config/config.js
+++ backend/auth-service/src/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     dialect: "mysql",
   },
   test: {
-    // Use separate variables or default to development for tests
+    // Use separate variables or default to development for test
     username: process.env.TEST_DB_USER || process.env.DB_USER,
     password: process.env.TEST_DB_PASSWORD || process.env.DB_PASSWORD,
     database: process.env.TEST_DB_NAME || process.env.DB_NAME,
